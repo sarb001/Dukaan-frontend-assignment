6cc974e4121229af6d77adf0be5297cbb32881e9
SARB <mrsinghbusiness05@gmail.com>
2024-04-15 21:05:56 +0530

    navbar corrected

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { AiTwotoneQuestionCircle } from "react-icons/ai";
 
+import { IoIosArrowDown } from "react-icons/io";
+
 const Dashboard = () => {
   return (
     <div>
-        <div> </div>
+        <div className='px-8 pt-9'>
+          <div className='flex justify-between'>
+             <div className='text-xl font-[500px]'> Overview </div>  
+             <div className='flex justify-center gap-4 w-[137px] h-9 rounded-md bg-lime-400 items-center  '>
+                <button> Last Month </button>
+                <span> <IoIosArrowDown /> </span>
+             </div>
+          </div>
+        </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import { AiOutlineQuestionCircle } from "react-icons/ai";
 import { TiArrowSortedDown } from "react-icons/ti";
+import { HiMagnifyingGlass } from "react-icons/hi2";
 
 
 const Navbar = () => {
   return (
-    <div className='flex flex-row justify-between  py-3 px-8 gap-4 bg-[#FFFFFF] '>
-        <div className='flex'>
-            <div> Payouts </div>
-            <div> <AiOutlineQuestionCircle />  </div>
+    <div className='flex flex-row justify-between border-b   py-3 px-8 gap-4 bg-[#FFFFFF] '>
+      
+        <div className='flex justify-between gap-1.5 items-center'>
+            <div> Payments </div>
+            <div className='flex justify-center gap-1.5 items-center' > 
+                 <AiOutlineQuestionCircle />
+                <p className='text-xs'> How it works </p> 
+             </div>
         </div>
      
-        <div> 
-            <input type= "text"  placeholder = 'Search feature,tutorials' />
+        <div className='flex gap-2 py-1.5  w-[400px] px-[9px] bg-[#F2F2F2] rounded-md items-center '> 
+            <HiMagnifyingGlass />
+            <input className='bg-[#F2F2F2] border-none' type= "text"  placeholder = 'Search feature,tutorials' />
         </div>
 
         <div className='flex flex-row gap-[10px] '>
