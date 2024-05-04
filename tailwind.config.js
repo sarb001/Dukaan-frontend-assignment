82b0623ff0b5c3a233d40917c20a5d1564f69581
SARB <mrsinghbusiness05@gmail.com>
2024-05-04 13:53:26 +0530

    extras removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,6 +8,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+  ],
 }
 
