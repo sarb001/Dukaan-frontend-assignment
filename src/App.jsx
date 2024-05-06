56df9c3733ab0c499fb8b7239d06f87d798d0ac4
SARB <mrsinghbusiness05@gmail.com>
2024-05-07 05:07:40 +0530

    icons added /reponsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 
 import './App.css'
+import Navbar from './Navbar'
+import Second from './Second'
 
 function App() {
 
   return (
-   <div className='flex flex-row  w-full'> 
-    <div className='w-[18%] bg-[#1E2640] h-[100vh]  text-white sticky left-0 top-0'>  
-     sidebar 
+   <div className='flex flex-row  w-full '> 
+    <div className='w-[0%] md:w-[18%]  bg-[#1E2640] h-[100vh]  text-white sticky left-0 top-0'>  
+      {/* sidebar 
+      sidebar 
+      sidebar 
+      sidebar 
+      sidebar 
+      sidebar 
+      sidebar 
+      sidebar  */}
     </div>
-    <div className='w-[82%] bg-[#FAFAFA]'>
-          Second
+    <div className='w-full  md:w-[82%] bg-[#FAFAFA]'>
+           <Navbar />
+           <Second />
     </div>
    </div>
   )
