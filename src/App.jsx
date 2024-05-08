9b4a0e5ac1dbdbfa21ed9103205053f98cb0bb21
SARB <mrsinghbusiness05@gmail.com>
2024-05-08 13:16:31 +0530

    dashboard responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@
 import './App.css'
 import Navbar from './Navbar'
 import Second from './Second'
-import { Analytics, Appearance, Audience, Delivery, Discounts, Downarrow, Home, MagnifyingGlass, Marketing, Orders, Payouts, Plugins, Products  } from '../public/assests/icons'
+import { Analytics, Appearance, Audience, CancelButton, Delivery, Discounts, Downarrow, Home, MagnifyingGlass, Marketing, Orders, Payouts, Plugins, Products  } from '../public/assests/icons'
+import { useState } from 'react'
 
 function App() {
 
@@ -19,17 +20,17 @@ function App() {
     {  label : 'Appearance' , icon : <Appearance /> , link : '#' },
     {  label : 'Plugins'    , icon : <Plugins /> , link : '#' },
 
-]
+  ]
 
-  //  const [menubutton,setmenubutton] = useState(false);
-
+   const [menubutton,setmenubutton] = useState(false);
 
   return (
    <div className='flex flex-row  w-full'> 
-
               {/*  whole side-bar  */}
                
-    <div className='hidden  tablet:block tablet:min-w-56  tablet:p-4 bg-[#1E2640] h-[100vh]  text-white '>  
+    <div className =  {` ${menubutton ? "block" : "hidden"} min-w-48 
+     z-30 fixed p-4
+     tablet:block tablet:min-w-56  tablet:p-4 bg-[#1E2640] h-[100vh]  text-white `}>  
 
             <div className='tablet:flex tablet:gap-3 tablet:py-[2px] tablet:px-[1px]'>
 
@@ -50,6 +51,7 @@ function App() {
 
             </div>
 
+            {/* All Icons */}
             <div className='tablet:mt-4'>
                {links?.map((i) => {
                  return (
@@ -63,64 +65,73 @@ function App() {
 
     </div>
 
+      { menubutton && 
+        <div className='tablet:hidden  absolute left-[250px]  bg-black
+        z-10 text-blue-600 ' 
+        onClick  = {() => setmenubutton(false)}>
+                    <CancelButton 
+                    />
+        </div>
+      }
 
-    <div className = 'w-full tablet:grow  bg-[#FAFAFA]'>
-          <div className='p-2 grid grid-cols-2 justify-between bg-[#ffff] border-b-[1px] border-[#d9d9d9] rounded-xl '>
 
-            <div className='flex items-center gap-5 '>
+      <div className = 'z-0 w-full tablet:grow  bg-[#FAFAFA]'>
+            <div className='p-2 grid grid-cols-2 justify-between bg-[#ffff] border-b-[1px] border-[#d9d9d9] rounded-xl '>
 
-                  {/* button hamburger */}
-                <div className=''>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
-                    </svg>
-                </div>
+              <div className='flex items-center gap-5'>
 
-                <div className='text-xl font-semibold '> Payouts  </div> 
+                    {/* button hamburger */}
+                  <div className='tablet:hidden relative' onClick={() => setmenubutton(!menubutton)}>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+                      </svg>
+                  </div>
 
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
-                    </svg>
-                </div>
+                  <div className='text-xl font-semibold '> Payouts  </div> 
 
-                <div className='text-xs'> How it works  </div> 
+                  <div>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
+                      </svg>
+                  </div>
 
-                {/*  input search bar */}
-                <div className='tablet:flex hidden  tablet:p-2 tablet:bg-slate-200 tablet:rounded-tablet tablet:grow '>
-                  <span> <MagnifyingGlass />  </span>
-                  <input  type = "text" placeholder='Search Features' className='tablet:bg-slate-200' />
-                </div>
+                  <div className='text-xs'> How it works  </div> 
 
-            </div>
+                  {/*  input search bar */}
+                  <div className='tablet:flex hidden  tablet:p-2 tablet:bg-slate-200 tablet:rounded-tablet tablet:grow '>
+                    <span> <MagnifyingGlass />  </span>
+                    <input  type = "text" placeholder='Search Features' className='tablet:bg-slate-200' />
+                  </div>
 
-            <div className='flex justify-end gap-2'>
+              </div>
 
-                <div className='bg-slate-200 p-2 rounded-full '>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                    </svg>
-                </div>
+              <div className='flex justify-end gap-2'>
 
-                <div className='bg-slate-200 p-2 rounded-full '>
+                  <div className='bg-slate-200 p-2 rounded-full '>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                      </svg>
+                  </div>
 
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
-                    </svg>
+                  <div className='bg-slate-200 p-2 rounded-full '>
 
-                </div>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+                      </svg>
 
-                <div className='bg-slate-200 p-2 rounded-full '>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-                    </svg>
-                </div>
+                  </div>
 
-            </div>
+                  <div className='bg-slate-200 p-2 rounded-full '>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                      </svg>
+                  </div>
 
-      </div>
-           <Second />
-    </div> 
+              </div>
+
+        </div>
+            <Second />
+      </div> 
 
 
    </div>
---
+++ src/MainNavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainNavbar = () => {
+  return (
+    <>
+     Navbar dd
+    </>
+  )
+}
+
+export default MainNavbar
