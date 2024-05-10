a58d5b72f22580e6a81829a6c063f9b8fced508c
SARB <mrsinghbusiness05@gmail.com>
2024-05-10 06:07:19 +0530

    from scratch -design

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,8 @@
+import React from 'react'
 
-import './App.css'
-import { Analytics, Appearance, Audience, CancelButton, Delivery, Discounts, Downarrow, Home, MagnifyingGlass, Marketing, Orders, Payouts, Plugins, Products  } from '../public/assests/icons'
-import { useState } from 'react'
-import Sidebar from './Sidebar'
-import Header from './Header'
-
-function App() {
-  
-   const [menubutton,setmenubutton] = useState(false);
-
+const App = () => {
   return (
-        <>
-          <div className = ' grid grid-rows-[auto_minmax(300px,_1fr)]  tablet:grid tablet:grid-cols-[224px_minmax(300px,_1fr)]'>
-            <div> 
-               <Sidebar  
-               menubutton = {menubutton}
-               setmenubutton = {setmenubutton} /> 
-            </div> 
-            <div> 
-               <Header   
-                  setmenubutton = {setmenubutton} 
-                  menubutton = {menubutton}
-             />   
-            </div>     
-          </div>
-        </>
-
-
+    <div>App</div>
   )
 }
 
