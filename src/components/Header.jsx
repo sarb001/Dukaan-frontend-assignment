b566c40b41d2312bb360c69af3fde30a509f89a7
SARB <mrsinghbusiness05@gmail.com>
2024-05-10 13:08:39 +0530

    in progress -dashboard

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,11 @@ const Header = () => {
     <div className='bg-[#fff] px-4 py-3 border-b-2 border-b-[#d9d9d9] grid grid-cols-2 gap-5'>
       
         <div className='flex items-center gap-2'>
-          <div className='w-[10%] tablet:hidden '> <HamburgerIcon /> </div>
+          <div className='w-[10%] tablet:hidden '> 
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+                  </svg>
+          </div>
           <span className='text-[#1A181E] text-xl font-medium'>  Payouts </span>
           <div className='flex gap-1.5 min-w-28 '> 
            <div className='w-[18%]'> <QuestionMark />   </div> 
